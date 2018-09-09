b058a71a9a815266cdb6eef4ac13b09fe48e7273
thewildvegetable <whichazelnj13@aol.com>
2018-09-09 17:25:59 -0400

    finish js

--- client/client.js
+++ client/client.js
@@ -19,6 +19,14 @@ const hideContent = () => {
     }
 };
 
+//erase all underlines
+const eraseUnderlines = () => {
+    let buttons = document.getElementsByClassName("contentButton");
+    for (let i = 0; i < buttons.length; i++){
+        buttons[i].style.textDecoration = "none";
+    }
+};
+
 //hide current project
 const hideProject = () => {
     let portfolioPiece = document.getElementsByClassName("portfolioPiece");
@@ -31,27 +39,60 @@ const init = () => {
   //get buttons
   aboutMenuButton = document.querySelector("#About");
   portfolioMenuButton = document.querySelector("#Portfolio");
-  aboutMenuButton = document.querySelector("#AboutContent");
-  aboutMenuButton = document.querySelector("#AboutContent");
-  aboutMenuButton = document.querySelector("#AboutContent");
-  aboutMenuButton = document.querySelector("#AboutContent");
+  contactMenuButton = document.querySelector("#Contact");
+  resumeMenuButton = document.querySelector("#Resume");
+  clickButton = document.querySelector("#projectClick");
+  bumpButton = document.querySelector("#projectBump");
+  showdownButton = document.querySelector("#projectShowdown");
+  dynamiteButton = document.querySelector("#projectDynamite");
+  idleButton = document.querySelector("#projectIdle");
     
   //set event listeners
-  lobbyButton.onclick = (e) => {
-      document.querySelector("#roomSelection").style.display = "block";
-      document.querySelector("#skins").style.display = "none";
+  aboutMenuButton.onclick = (e) => {
+      hideContent();
+      eraseUnderlines();
+      aboutMenuButton.style.textDecoration = "underline";
+      document.querySelector("#AboutContent").style.display = "block";
   };
-  skinButton.onclick = (e) => {
-      document.querySelector("#roomSelection").style.display = "none";
-      document.querySelector("#skins").style.display = "block";
+  portfolioMenuButton.onclick = (e) => {
+      hideContent();
+      eraseUnderlines();
+      portfolioMenuButton.style.textDecoration = "underline";
+      document.querySelector("#PortfolioContent").style.display = "block";
   };
-  buyButton.onclick = purchaseSkin;
-  equipButton.onclick = equipSkin;
-  closeButton.onclick = (e) => {
-      document.querySelector("#unsuccessfulEquip").style.display = "none";
+  contactMenuButton.onclick = (e) => {
+      hideContent();
+      eraseUnderlines();
+      contactMenuButton.style.textDecoration = "underline";
+      document.querySelector("#ContactContent").style.display = "block";
   };
-
-  
+  resumeMenuButton.onclick = (e) => {
+      hideContent();
+      eraseUnderlines();
+      resumeMenuButton.style.textDecoration = "underline";
+      document.querySelector("#ResumeContent").style.display = "block";
+  };
+    
+  clickButton.onclick = (e) => {
+      hideProject();
+      document.querySelector("#portfolioClick").style.display = "block";
+  };
+  bumpButton.onclick = (e) => {
+      hideProject();
+      document.querySelector("#portfolioBump").style.display = "block";
+  };
+  showdownButton.onclick = (e) => {
+      hideProject();
+      document.querySelector("#portfolioShowdown").style.display = "block";
+  };
+  dynamiteButton.onclick = (e) => {
+      hideProject();
+      document.querySelector("#portfolioDynamite").style.display = "block";
+  };
+  idleButton.onclick = (e) => {
+      hideProject();
+      document.querySelector("#portfolioIdle").style.display = "block";
+  };
 };
 
 window.onload = init;
